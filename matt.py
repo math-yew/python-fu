389e31800eee6bef5c83f471c86c970b29d6b663
math-yew <math.u@hotmail.com>
2020-04-18 16:40:12 -0600

    rainbox layers

--- matt.py
+++ matt.py
@@ -4,31 +4,41 @@ from gimpfu import *
 
 
 def python_selection_to_layer_cropped(image, drawable, num):
-    pdb.gimp_image_undo_group_start(image)
-    prev_layer = image.active_layer
-
+    # pdb.gimp_image_undo_group_start(image)
+    first_layer = image.active_layer
+    colors = ["white","purple","green","blue","red","orange","cyan","yellow"]
     pdb.gimp_edit_copy(image.active_layer)
     i = 1
     layer_num = int(num)+1
     # layer_num = 5
     while i < layer_num:
-      print(i)
       fsel = pdb.gimp_edit_paste(drawable, False)
       pdb.gimp_floating_sel_to_layer(fsel)
+      prev_layer = image.active_layer
       # pdb.gimp_message(potato)
       low_threshold = round(.5+.5*(math.log(i)/math.log(layer_num)),2)
       pdb.gimp_drawable_threshold(image.active_layer, 0, low_threshold, 1)
+      pdb.plug_in_colortoalpha(image, image.active_layer, "black")
 
-pdb.plug_in_colortoalpha(image, drawable, color)
-pdb.gimp_image_add_layer(image, layer, position)
-position = pdb.gimp_image_get_item_position(image, item)
-layer = pdb.gimp_image_merge_down(image, merge_layer, 0)
-pdb.gimp_bucket_fill(drawable, 0, 0, 100, 255, FALSE, 1, 1)
+      fsel = pdb.gimp_edit_paste(drawable, False)
+      pdb.gimp_floating_sel_to_layer(fsel)
+      pdb.gimp_drawable_edit_clear(image.active_layer)
+      pdb.gimp_palette_set_foreground(colors[i])
+      pdb.gimp_bucket_fill(image.active_layer, 0, 0, 100, 255, TRUE, 1, 1)
+      # pdb.gimp_drawable_edit_bucket_fill(image.active_layer, 1, 1, 1)
+      pdb.gimp_image_merge_down(image, prev_layer, 0)
+      pdb.plug_in_colortoalpha(image, image.active_layer, "white")
+
+# pdb.plug_in_colortoalpha(image, drawable, color)
+# pdb.gimp_image_add_layer(image, layer, position)
+# position = pdb.gimp_image_get_item_position(image, item)
+# layer = pdb.gimp_image_merge_down(image, merge_layer, 0)
+# pdb.gimp_bucket_fill(drawable, 0, 0, 100, 255, FALSE, 1, 1)
 
       i += 1
-    image.active_layer = prev_layer
+    image.active_layer = first_layer
 
-    pdb.gimp_image_undo_group_end(image)
+    # pdb.gimp_image_undo_group_end(image)
 
 
 register(
